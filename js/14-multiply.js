3d5e4672abb92ab119cb9d723b4e09c8c1e2af41
Spartakas9 <79445360+Spartakas9@users.noreply.github.com>
2021-03-17 00:47:54 +0200

    Sandauga

---
+++ js/14-multiply.js
@@ -0,0 +1,10 @@
+function daugyba (a, b) {
+    const rez = a * b;
+    return rez;
+}
+
+console.log(daugyba(4, 5));
+console.log(daugyba(44, 5));
+console.log(daugyba(-44, -55));
+console.log(daugyba(0, 0));
+console.log(daugyba(0, 54850));
