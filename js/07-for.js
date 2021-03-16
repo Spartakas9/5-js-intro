23c30f00033d9be47be680ef2761e37c15750d66
Spartakas9 <79445360+Spartakas9@users.noreply.github.com>
2021-03-16 23:10:03 +0200

    Suma intervale, atbulas tekstas, liekanu kiekis

--- js/07-for.js
+++ js/07-for.js
@@ -27,9 +27,27 @@ console.log('ciklo pabaiga');
 
 console.log('----------------------');
 
-const marks = [5, 7, 10, 4, 2, 10];
+const zmones = ['Petras', 'Maryte', 'Jonas', 'Onute', 'Elvyra'];
 
+for ( let i = 0; i<zmones.length; i++) {
+    const asmuo = zmones[i];
 
-for (let i = 0; i < 6; i++) {
-    console.log(marks[i]);
+    console.log(asmuo);
 }
+
+console.log('----------------------');
+
+const marks = [5, 7, 10, 4, 2, 10, 8, 9, 1, 3];
+let sum = 0;
+
+for (let i = 0; i < marks.length; i++) {
+    const pazymys = marks[i];
+    sum += pazymys;
+}
+
+const average = sum / marks.length;
+
+console.log(sum, '/', marks.length, '=', average);
+
+console.log('----------------------');
+
