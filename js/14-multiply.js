785c5be36958d8d070fa15b22de102ce3169fd9c
Spartakas9 <79445360+Spartakas9@users.noreply.github.com>
2021-03-19 20:11:52 +0200

    Update

--- js/14-multiply.js
+++ js/14-multiply.js
@@ -3,8 +3,14 @@ function daugyba (a, b) {
     return rez;
 }
 
-console.log(daugyba(4, 5));
-console.log(daugyba(44, 5));
-console.log(daugyba(-44, -55));
-console.log(daugyba(0, 0));
-console.log(daugyba(0, 54850));
+console.log(daugyba(4, 5), '->', 20);
+console.log(daugyba(44, 5), '->', 220);
+console.log(daugyba(-44, -55), '->', 2420);
+console.log(daugyba(-44, 55), '->', -2420);
+console.log(daugyba(-44, 0), '->', 0);
+console.log(daugyba(0, 0), '->', 0);
+console.log(daugyba(0, 54850), '->', 0);
+console.log(daugyba(3.14, 2), '->', 6.28);
+console.log(daugyba(3, 5), '->', 15);
+
+console.log(daugyba(3, 5), '->', 15);
