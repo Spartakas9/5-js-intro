603716229b06de901c849ba35bef73dbaa73a5a3
Spartakas9 <79445360+Spartakas9@users.noreply.github.com>
2021-03-20 18:00:43 +0200

    daugybos testai

--- js/14-multiply.js
+++ js/14-multiply.js
@@ -1,12 +1,18 @@
 function daugyba (a, b) {
     // parametru validacija
-    // jeigu, a yra ne skaicius, grazinti klaidos pranesima
+    
     if (typeof a !== 'number') {
         return 'Pirmasis parametras turi buti skaiciaus tipo';
     }
-    // jeigu, b yra ne skaicius, grazinti klaidos pranesima
+    if (isNaN(a) || a === Infinity || a === -Infinity) {
+        return `Pirmasis parametras turi buti normalus skaiciaus ir negali buti ${a}`;
+    }
+    
     if (typeof b !== 'number') {
         return 'Antrasis parametras turi buti skaiciaus tipo';
+    }
+    if (isNaN(b) || b === Infinity || b === -Infinity) {
+        return `Antrasis parametras turi buti normalus skaiciaus ir negali buti ${b}`;
     }
 
     // logika
@@ -26,7 +32,23 @@ console.log(daugyba(false, 5));
 console.log(daugyba(['asd', 786], 3));
 console.log(daugyba(3, ['asd', 786]));
 console.log(daugyba(['asd', 786], ['asd', 786]));
+console.log(daugyba(['asd', 786], 3));
+console.log(daugyba(3, ['asd'], 786));
+console.log(daugyba(['asd'], ['asd']));
+console.log(daugyba([2], 3));
+console.log(daugyba(3, [2]));
+console.log(daugyba([2], [2]));
+console.log(daugyba(NaN, 3));
+console.log(daugyba(3, NaN));
+console.log(daugyba(NaN, NaN));
+console.log(daugyba(Infinity, 3));
+console.log(daugyba(3, Infinity));
+console.log(daugyba(Infinity, Infinity));
+console.log(daugyba(-Infinity, 3));
+console.log(daugyba(3, -Infinity));
+console.log(daugyba(-Infinity, -Infinity));
 
+console.log('----------------------------------------------');
 
 console.log(daugyba(4, 5), '->', 20);
 console.log(daugyba(44, 5), '->', 220);
